4b7237984576e236433a29419f17fce353887802
bates232 <bates232@unicaen.fr>
2024-09-23 16:15:35 +0200

    projet_pomodoro copie de l'ancien projet: -update css pour correspondre à la maquette

---
+++ js/pomodoro.js
@@ -0,0 +1,140 @@
+/**
+ * Variable declaration
+ */
+
+
+const body = document.body
+
+const inputWorkDuration = document.getElementById('work-duration')
+const inputRestDuration = document.getElementById('rest-duration')
+
+const timerTime = document.getElementById('timer-time')
+const circleProgress = document.querySelector('.circle-progress')
+
+let workDuration = inputWorkDuration.value * 60
+let restDuration = inputRestDuration.value * 60
+
+let isPaused = true
+let isWorking = true
+let remainingTime = workDuration
+
+funcUpdateProgress()
+
+/**
+ * Update Settings
+ */
+
+inputWorkDuration.addEventListener('change', () => {
+    workDuration = inputWorkDuration.value * 60
+    if(isWorking) {
+        remainingTime = workDuration
+        funcUpdateProgress()
+    }
+})
+    
+inputRestDuration.addEventListener('change', () => {
+    restDuration = inputRestDuration.value * 60
+    if(isWorking) {
+        remainingTime = workDuration
+        funcUpdateProgress()
+    }
+})
+
+/**
+ * Button 
+ */
+
+const startBtn = document.getElementById('btn-start')
+startBtn.addEventListener('click', () => {
+    isPaused = false
+
+    if(isWorking) {
+        body.classList.add('timer-running')
+        body.classList.remove('timer-paused')
+    } else {
+        body.classList.add('rest-mode');
+        body.classList.remove('timer-paused')
+    }
+
+    setInterval(funcUpdateTimer, 1000)
+})
+
+const pauseBtn = document.getElementById('btn-pause')
+pauseBtn.addEventListener('click', () => {
+    isPaused = true
+    
+    body.classList.remove('timer-running')
+    body.classList.add('timer-paused')
+})
+
+const resetBtn = document.getElementById('btn-reset')
+resetBtn.addEventListener('click', () => {
+    location.reload()
+})
+
+const settingsBtn = document.getElementById('btn-settings');
+settingsBtn.addEventListener('click', () => {
+    body.classList.add('settings-open');
+});
+
+const closeBtn = document.getElementById('btn-close')
+closeBtn.addEventListener('click', () => {
+    body.classList.remove('settings-open')
+})
+
+/**
+ * Function
+ */
+
+function funcUpdateTimer() {
+    if(!isPaused) {
+        remainingTime--
+
+        if(remainingTime <= 0) {
+            isWorking = !isWorking
+
+            if (isWorking) {
+                remainingTime = workDuration;
+                body.classList.remove('rest-mode')
+                body.classList.add('timer-running')
+            } else {
+                remainingTime = restDuration
+                body.classList.add('rest-mode')
+                body.classList.remove('timer-running')
+
+            }
+
+            isPaused = false
+        }
+        
+        funcUpdateProgress();
+    }
+}
+
+function funcUpdateProgress() {
+    const radius = 45
+    const circumference = 2 * Math.PI * radius
+
+    let totalDuration;
+
+    if (isWorking) {
+        totalDuration = workDuration;
+    } else {
+        totalDuration = restDuration;
+    }
+
+    const dashOffset = circumference * remainingTime / totalDuration
+
+    circleProgress.style.strokeDashoffset = dashOffset
+    timerTime.textContent = toRightFormatTime(remainingTime);
+}
+
+function funcSettingsChanged(){
+
+}
+
+function toRightFormatTime(sec) {
+    const min = Math.floor(sec / 60)
+    const remainingSec = sec % 60
+    return `${min.toString().padStart(2, '0')}:${remainingSec.toString().padStart(2, '0')}`
+}
